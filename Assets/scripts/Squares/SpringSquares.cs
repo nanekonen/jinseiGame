d0cb0fcb3f347973987c1ca380b28d882d1929d6
maejima <77713347+kisiberohann@users.noreply.github.com>
2023-11-21 16:31:23 +0900

    ちょっと修正

--- Assets/scripts/Squares/SpringSquares.cs
+++ Assets/scripts/Squares/SpringSquares.cs
@@ -7,17 +7,7 @@ using TMPro;
 
 public class SpringSquares : SeasonSquares
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public  override List<Square> changeOfSquares(List<RealSquare> rsquares)
     {
 
