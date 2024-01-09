5f19093d0ff5416be2f91c80fed58800f56f8720
maejima <77713347+kisiberohann@users.noreply.github.com>
2024-01-09 14:46:37 +0900

    改良

--- Assets/scripts/Love/Lover.cs
+++ Assets/scripts/Love/Lover.cs
@@ -16,25 +16,32 @@ public class Lover
 
     public Favorability fav = Favorability.UNDEFINED_FAVORABILITY;
 
-    public Lover(string name, string fullName, Favorability fav)
+    public Lover(string name, string fullName, Favorability fav,Gender g,string path)
     {
         this.name = name;
         this.fullName = fullName;
         this.fav = fav;
-        //loadImage(g, a);
+        loadImage(g, path);
     }
 
-    /*
-    private async void loadImage(Gender g,Activity a)
+    
+    private async void loadImage(Gender g,string path)
     {
-        string p = "image_subject/" + (g == Gender.MAN ? "boy" : "girl") + "_" + a.ToString("0");
+        /*
+        string[] ar_act =
+        {
+            "basketball_club","brass-band_club","part-time_job"
+        };
+        */
+        string p = "image_subject/" + (Gender.WOMAN == g?"girl":"boy") + "_" + path;
+        Debug.Log(p);
         Sprite s;
         normal = s = await Addressables.LoadAssetAsync<Sprite>(p + "_normal.jpg").Task;
         Addressables.Release(s);
         privat = s = await Addressables.LoadAssetAsync<Sprite>(p + "_private.jpg").Task;
         Addressables.Release(s);
     }
-    */
+    
 
     public string getName()
     {
